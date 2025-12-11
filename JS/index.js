c0a9744bce378344060ea6f39aee4d3f766ddeb0
Rupert Foley <rupert.foley@gmail.com>
2025-12-11 23:58:14 +0000

    Added in nav menu button for low page widths, made h1 dip slightly at very small page widths to avoid the menu button overlapping it. Little bit of tidy up

--- JS/index.js
+++ JS/index.js
@@ -6,3 +6,26 @@ glitchText.forEach(element => {
     element.setAttribute("data-text", element.innerText);
     element.innerHTML = "<span>" + element.innerHTML + "</span>";
 });
+
+const menuButton = document.querySelector('#menu-button');
+const navBar = document.querySelector('.nav-bar');
+let menuOpen = false;
+
+menuButton.addEventListener('click', toggleMenu);
+window.onresize = removeMenuStyle;
+function toggleMenu() {
+    if(menuOpen){
+        navBar.style.right = "-240px";
+        menuOpen = false;
+    } else {
+        navBar.style.right = "0";
+        menuOpen = true;
+    }
+}
+
+function removeMenuStyle(){
+    console.log(window.innerWidth);
+    if(window.innerWidth < 800) return;
+    navBar.style = "";
+    menuOpen = false;
+}
