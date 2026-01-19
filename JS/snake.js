b167eb566cbc08b1f51b6dfdd5467569402360a7
Rupert Foley <rupert.foley@gmail.com>
2026-01-19 21:57:55 +0000

    Refactored snake game logic to fix movement issues and improve game state handling; adjusted link styling in games page for better layout consistency. Added in ability to restart the game while playing.

--- JS/snake.js
+++ JS/snake.js
@@ -1,11 +1,9 @@
-﻿//todo: if you go a different direction and then back in the opposite direction before the next frame is drawn it goes back on itself and game over
-
-const canvas = document.querySelector("#canvas");
+﻿const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 const easyButton = document.querySelector("#easy");
 const mediumButton = document.querySelector("#medium");
 const hardButton = document.querySelector("#hard");
-const fps = 10;
+let fps = 10;
 let blockSize = 25;
 let totalRows = 17;
 let totalColumns = 17;
@@ -15,32 +13,41 @@ let score = 0;
 let snakeX = blockSize * 5;
 let snakeY = blockSize * 5;
 
-let speed = { x: 0, y: 0}
+let speed = { x: 0, y: 0};
+let nextSpeed = { x: 0, y: 0};
+let lastSpeed = { x: 0, y: 0};
 
 let snake = [];
 
 let foodCoords = { x: 0, y: 0 };
 
 let gameOver = false;
+let changedDirection = false;
+let playing = false;
 
 function startGame(){
     gameOver = false;
     score = 0;
     snake = [];
-    speed = { x: 0, y: 0}
+    nextSpeed = { x: 0, y: 0};
+    speed = { x: 0, y: 0};
+    lastSpeed = { x: 0, y: 0};
     snakeX = blockSize * 5;
     snakeY = blockSize * 5;
     canvas.height = blockSize * totalRows;
     canvas.width = blockSize * totalColumns;
     placeFood();
     document.addEventListener("keydown", changeDirection);
-    draw();
+    if(!playing) {
+        playing = true;
+        console.log("starting game");
+        draw();
+    }
 }
 
-
 function draw(){
-    if(gameOver) return;
-
+    if(gameOver || !playing) return;
+    changedDirection = true;
     // Background of a Game
     ctx.fillStyle = "#9744be";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -65,37 +72,41 @@ function draw(){
     }
 
     ctx.fillStyle = "white";
-    snakeX += speed.x * blockSize; //updating Snake position in X coordinate.
-    snakeY += speed.y * blockSize;  //updating Snake position in Y coordinate.
+
+    speed = nextSpeed;
+    snakeX += speed.x * blockSize;
+    snakeY += speed.y * blockSize;
+
     ctx.fillRect(snakeX, snakeY, blockSize, blockSize);
     for (let i = 0; i < snake.length; i++) {
         ctx.fillRect(snake[i][0], snake[i][1], blockSize, blockSize);
     }
 
     if (snakeX < 0
-        || snakeX > totalColumns * blockSize
+        || snakeX >= totalColumns * blockSize
         || snakeY < 0
-        || snakeY > totalRows * blockSize) {
-
-        // Out of bound condition
-        gameOver = true;
-        alert("Game Over");
+        || snakeY >= totalRows * blockSize) {
+        endGame();
     }
 
     for (let i = 0; i < snake.length; i++) {
         if (snakeX === snake[i][0] && snakeY === snake[i][1]) {
-
-            // Snake eats own body
-            gameOver = true;
-            alert("Game Over");
+            endGame();
         }
     }
+
+    changedDirection = false;
     setTimeout( () =>
     {
         requestAnimationFrame(draw);
     }, 1000/fps)
 }
 
+function endGame(){
+    gameOver = true;
+    playing = false;
+    alert("Game Over");
+}
 
 function placeFood(){
     foodCoords = {
@@ -104,46 +115,54 @@ function placeFood(){
     }
 }
 
-
-//todo: add in check to see if turning in that direction causes you to go into the section behind the head, skip if true
 function changeDirection(e){
+    if(changedDirection) return;
     switch(e.code){
         case "ArrowUp":
-            if(speed.y === 1) return;
-            speed.y = -1;
-            speed.x = 0;
+            if(lastSpeed.y === 1) break;
+            nextSpeed.y = -1;
+            nextSpeed.x = 0;
             break;
         case "ArrowDown":
-            if(speed.y === -1) return;
-            speed.y = 1;
-            speed.x = 0;
+            if(lastSpeed.y === -1) break;
+            nextSpeed.y = 1;
+            nextSpeed.x = 0;
             break;
         case "ArrowLeft":
-            if(speed.x === 1) return;
-            speed.y = 0;
-            speed.x = -1;
+            if(lastSpeed.x === 1) break;
+            nextSpeed.y = 0;
+            nextSpeed.x = -1;
             break;
         case "ArrowRight":
-            if(speed.x === -1) return;
-            speed.y = 0;
-            speed.x = 1;
+            if(lastSpeed.x === -1) break;
+            nextSpeed.y = 0;
+            nextSpeed.x = 1;
             break;
         default:
             break;
     }
 }
 
-
 easyButton.addEventListener("click", () => {
+    fps = 10;
+    blockSize = 25;
+    totalRows = 17;
+    totalColumns = 17;
     startGame();
 });
 
-
 mediumButton.addEventListener("click", () => {
-
+    fps = 15;
+    blockSize = 25;
+    totalRows = 21;
+    totalColumns = 21;
     startGame();
 })
 
 hardButton.addEventListener("click", () => {
+    fps = 20;
+    blockSize = 25;
+    totalRows = 25;
+    totalColumns = 25;
     startGame();
 })
